c70e4545033d299be5fbf646b1707a21a061b32d
ExpLangcn <616335643@qq.com>
2022-03-11 17:23:23 +0800

    FuYao - 扶摇直上九万里！

---
+++ README.md
@@ -0,0 +1,48 @@
+# FuYao - 扶摇直上九万里
+
+**[WanLi](https://github.com/ExpLangcn/WanLi)** ｜[LICENSE](LICENSE) ｜ **[Twitter](https://twitter.com/ExpLang_Cn)** 
+
+**自动化进行资产探测及漏洞扫描｜适用黑客进行赏金活动、SRC活动、大规模攻击使用**
+
+# 使用
+
+```
+git clone https://github.com/ExpLangcn/FuYao.git
+cd FuYao & pip3 install -r requirements.txt
+vim config.yaml
+```
+**[Help](https://github.com/ExpLangcn/WanLi/wiki/Help---%E5%B8%AE%E5%8A%A9)**
+
+```
+python3 FuYao.py
+```
+# 帮助
+
+`domain.txt` 存放目标一级域名（主域名）
+
+`config.yaml` 配置扫描器
+
+**注：**目前工具仅限支持Mac系统及Linux系统，建议使用Linux系统！扫描速度与网络有关，建议VPS最少5MB宽带。
+
+# BiLiBiLi
+
+**[RedCodeTm](https://www.bilibili.com/)**
+
+# Twitter
+
+**[@ExpLang_Cn](https://twitter.com/ExpLang_Cn)**
+
+# 微信群
+
+![WechatIMG455.jpeg](img/WechatIMG455.jpeg)
+
+# 作者微信
+
+![WechatIMG408](img/WechatIMG408.jpeg)
+
+# Info
+
+* **[ksubdomain](https://github.com/knownsec/ksubdomain)**
+* **[subfinder](https://github.com/projectdiscovery/subfinder)**
+* **[httpx](https://github.com/projectdiscovery/httpx)**
+* **[nuclei](https://github.com/projectdiscovery/nuclei)**
---
+++ core/domain.py
@@ -0,0 +1,151 @@
+import os
+import sys
+import csv
+import yaml
+import time
+from rich.console import Console
+from rich.table import Column, Table
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+import core.rprint as rprint
+console = Console()
+with open('./config.yaml','r', encoding='utf-8') as f:
+    config = yaml.load(f, Loader=yaml.CLoader)
+
+def subfinder(target, date):
+    rprint.info(date, '正在调用subfinder进行子域名探测任务！')
+    time.sleep(1)
+    if config['system'] == 'mac':
+        os.system('./core/plus/mac/subfinder -list %s  -t 100 -all -o ./logs/subfinder/%s -silent'%(target, date + '_subfinder.txt'))
+        rprint.success(date, '已完成subfinder子域名探测任务！')
+        time.sleep(1)
+    if config['system'] == 'linux':
+        os.system('./core/plus/linux/subfinder -list %s -t 100 -all -o ./logs/subfinder/%s -silent'%(target, date + '_subfinder.txt'))
+        rprint.success(date, '已完成subfinder子域名探测任务！')
+        time.sleep(1)
+
+def ksubdomain(target, date):
+    rprint.info(date, '正在调用ksubdomain进行子域名探测任务！')
+    time.sleep(1)
+    if config['system'] == 'mac':
+        os.system('./core/plus/mac/ksubdomain enum --band 1G -dl %s --skip-wild --silent --only-domain --level %s --retry 1 --output logs/ksubdomain/%s'%(target, config['level'], date + '_ksubdomain.txt'))
+        rprint.success(date, '已完成ksubdomain子域名探测任务！')
+        time.sleep(1)
+    if config['system'] == 'linux':
+        os.system('./core/plus/linux/ksubdomain enum --band 1G -dl %s --skip-wild --silent --only-domain --level %s --retry 1 --output logs/ksubdomain/%s'%(target, config['level'], date + '_ksubdomain.txt'))
+        rprint.success(date, '已完成ksubdomain子域名探测任务！')
+        time.sleep(1)
+
+def httpx(date):
+    rprint.info(date, '正在调用httpx进行域名验证任务！')
+    time.sleep(1)
+    lst = []
+    result = []
+    domain = open('logs/domain.txt', 'w')
+    with open('logs/ksubdomain/%s_ksubdomain.txt'%(date), 'r') as f:
+        for line in f:
+            lst.append(line)
+    f.close()
+    with open('logs/subfinder/%s_subfinder.txt'%(date), 'r') as f:
+        for line in f:
+            lst.append(line.strip('\n'))
+    f.close()
+    for item in lst:
+        if not item in result:
+            result.append(item.strip('\n'))
+
+    for line in result:
+        domain.write(line+'\n')
+    f.close()
+    
+    if config['system'] == 'mac':
+        os.system('./core/plus/mac/httpx -l logs/domain.txt -cdn -ec -content-length -title -tech-detect -status-code -threads 200 -silent -csv -o logs/httpx/%s.csv'%(date + '_domain'))
+        table = Table(show_header=True)
+        table.add_column("ID", style="dim")
+        table.add_column("IP")
+        table.add_column('CDN')
+        table.add_column("Url")
+        table.add_column("Port")
+        table.add_column("Title")
+        table.add_column("Code")
+        table.add_column("Length")
+        table.add_column("Technologies")
+        csv_reader = csv.reader(open("logs/httpx/%s_domain.csv".encode('latin-1').decode('utf-8')%(date)))
+        ii = 0
+        Output = open('result/domain.txt',mode='w', encoding="utf-8")
+        for line in csv_reader:
+            Url = line[10]
+            if 'http' in Url:
+                ID = ii
+                IP = line[19]
+                Port = line[4]
+                Url = line[10]
+                Title = line[13]
+                Length = line[20]
+                Code = line[22]
+                CDN = line[29]
+                Technologies = line[31]
+                Output.write('%s\n'%(Url))
+                table.add_row(
+                    str(ii),
+                    str(IP),
+                    str(CDN),
+                    str(Url),
+                    str(Port),
+                    str(Title),
+                    str(Code),
+                    str(Length),
+                    str(Technologies.strip('[').strip(']'))
+                    )
+                ii += 1
+            else:
+                continue
+        Output.close()
+        console.print(table)
+        rprint.success(date, '已完成httpx域名验证任务！')
+    if config['system'] == 'linux':
+        os.system('./core/plus/linux/httpx -l logs/domain.txt -cdn -ec -content-length -title -tech-detect -status-code -threads 200 -silent -csv -o logs/httpx/%s.csv'%(date + '_domain'))
+        table = Table(show_header=True)
+        table.add_column("ID", style="dim")
+        table.add_column("IP")
+        table.add_column('CDN')
+        table.add_column("Url")
+        table.add_column("Port")
+        table.add_column("Title")
+        table.add_column("Code")
+        table.add_column("Length")
+        table.add_column("Technologies")
+        csv_reader = csv.reader(open("logs/httpx/%s_domain.csv"%(date), encoding="utf-8"))
+        ii = 0
+        Output = open('result/domain.txt',mode='w', encoding="utf-8")
+        for line in csv_reader:
+            Url = line[10]
+            if 'http' in Url:
+                ID = ii
+                IP = line[19]
+                Port = line[4]
+                Url = line[10]
+                Title = line[13]
+                Length = line[20]
+                Code = line[22]
+                CDN = line[29]
+                Technologies = line[31]
+                Output.write('%s\n'%(Url))
+                table.add_row(
+                    str(ii),
+                    str(IP),
+                    str(CDN),
+                    str(Url),
+                    str(Port),
+                    str(Title),
+                    str(Code),
+                    str(Length),
+                    str(Technologies.strip('[').strip(']'))
+                    )
+                ii += 1
+            else:
+                continue
+        Output.close()
+        console.print(table)
+        rprint.success(date, '已完成httpx域名验证任务！')
